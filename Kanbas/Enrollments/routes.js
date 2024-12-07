d954e496ef191157755ed91b1c8fe6feb2752492
Hao Tsai <o@Haos-MacBook-Air.local>
2024-12-07 04:49:05 -0500

    new

--- Kanbas/Enrollments/routes.js
+++ Kanbas/Enrollments/routes.js
@@ -1,6 +1,27 @@
 import * as enrollmentsDao from './dao.js';
 
 export default function EnrollmentsRoutes(app) {
+  const enrollUserInCourse = async (req, res) => {
+    let { uid, cid } = req.params;
+    if (uid === "current") {
+      const currentUser = req.session["currentUser"];
+      uid = currentUser._id;
+    }
+    const status = await enrollmentsDao.enrollUserInCourse(uid, cid);
+    res.send(status);
+  };
+  const unenrollUserFromCourse = async (req, res) => {
+    let { uid, cid } = req.params;
+    if (uid === "current") {
+      const currentUser = req.session["currentUser"];
+      uid = currentUser._id;
+    }
+    const status = await enrollmentsDao.unenrollUserFromCourse(uid, cid);
+    res.send(status);
+  };
+  app.post("/api/users/:uid/courses/:cid", enrollUserInCourse);
+  app.delete("/api/users/:uid/courses/:cid", unenrollUserFromCourse);
+ 
   app.post('/api/enrollments', (req, res) => {
     const { userId, courseId } = req.body;
     enrollmentsDao.enrollUserInCourse(userId, courseId);
